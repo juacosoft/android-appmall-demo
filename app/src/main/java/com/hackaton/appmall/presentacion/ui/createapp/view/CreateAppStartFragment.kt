321fe731a4f236b009e9087a09c850ff11a95e39
Nisum-exjomam <joaquin.martinezm@externos.bci.cl>
2023-06-06 14:53:10 -0500

    Se agrega activity create app y fragment base

---
+++ app/src/main/java/com/hackaton/appmall/presentacion/ui/createapp/view/CreateAppStartFragment.kt
@@ -0,0 +1,16 @@
+package com.hackaton.appmall.presentacion.ui.createapp.view
+
+import com.hackaton.appmall.databinding.FragmentCreateAppstartBinding
+import com.hackaton.appmall.presentacion.BaseFragment
+
+class CreateAppStartFragment : BaseFragment<FragmentCreateAppstartBinding> (
+    FragmentCreateAppstartBinding::inflate
+) {
+    override fun onViews() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onViewModels() {
+        TODO("Not yet implemented")
+    }
+}
