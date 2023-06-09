bde0b2139ba23ffabda9dc32265eed5ff30df396
Nisum-exjomam <joaquin.martinezm@externos.bci.cl>
2023-06-08 20:27:27 -0500

    mock home y menus header

--- app/src/main/java/com/hackaton/appmall/presentacion/ui/home/view/MainActivity.kt
+++ app/src/main/java/com/hackaton/appmall/presentacion/ui/home/view/MainActivity.kt
@@ -1,23 +1,58 @@
 package com.hackaton.appmall.presentacion.ui.home.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.PopupMenu
+import androidx.appcompat.app.AppCompatActivity
 import com.hackaton.appmall.R
-import com.hackaton.appmall.databinding.ActivityCreateAppBinding
+import com.hackaton.appmall.databinding.ActivityMainBinding
 import com.hackaton.appmall.presentacion.ui.createapp.startstep.view.CreateAppActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private val binding: ActivityCreateAppBinding by lazy {
-        ActivityCreateAppBinding.inflate(layoutInflater)
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         // TODO call activity temporal
-        startActivity(CreateAppActivity.createIntent(this))
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.person -> {
+                    // Handle favorite icon press
+                    true
+                }
+                R.id.menu -> {
+                    val menuItemView = findViewById<View>(R.id.menu)
+                    showPopupMenu(menuItemView)
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+
+    private fun showPopupMenu(anchor: View) {
+        val popupMenu = PopupMenu(this, anchor)
+        popupMenu.inflate(R.menu.menu_hamburgesa_home)
+        popupMenu.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.menu_home_createapp -> {
+                    startActivity(CreateAppActivity.createIntent(this))
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+        popupMenu.show()
+
     }
 }
